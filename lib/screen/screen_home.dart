7eabad1a6cea9ae3dd2668eed924cfb6b97c103c
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-08-12 22:59:12 +0900

    #2 - 퀴즈 모델 만들고 퀴즈 풀기 화면 만들기

--- lib/screen/screen_home.dart
+++ lib/screen/screen_home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz_project/model/model_quiz.dart';
+import 'package:quiz_project/screen/screen_quiz.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -7,83 +9,112 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Quiz> quizes = [
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0
+    }),
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0
+    }),
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0
+    }),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
+    Size screenSize = MediaQuery
+        .of(context)
+        .size;
     double width = screenSize.width;
     double height = screenSize.height;
 
     return SafeArea(
-        child: Scaffold(
-          appBar : AppBar(
-            title: Text('My Quiz App'),
-            backgroundColor: Colors.deepPurple,
-            leading: Container(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('My Quiz App'),
+          backgroundColor: Colors.deepPurple,
+          leading: Container(),
         ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Center(child: Image.asset(
-                'images/quiz_icon.jpg',
-                width: width*0.4,
-                ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(child: Image.asset(
+              'images/quiz_icon.jpg',
+              width: width * 0.4,
+            ),
+            ),
+            Padding(padding: EdgeInsets.all(width * 0.024),
+            ),
+            Text(
+              '플러터 퀴즈 앱',
+              style: TextStyle(
+                fontSize: width * 0.065,
+                fontWeight: FontWeight.bold,
               ),
-              Padding(padding: EdgeInsets.all(width*0.024),
-              ),
-              Text(
-                '플러터 퀴즈 앱',
-                style: TextStyle(
-                  fontSize: width*0.065,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                '퀴즈를 풀기 전 안내사항입니다. \n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요',
-                textAlign: TextAlign.center,
-              ),
-              Padding(
-                padding: EdgeInsets.all(width*0.048),
-              ),
-              _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요'),
-              _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤\n다음 문제 버튼을 눌러주세요.'),
-              _buildStep(width, '3. 만점을 향해 도전해보세요!'),
-              Padding(
-                padding: EdgeInsets.all(width*0.048),
-              ),
-              Container(
-                padding: EdgeInsets.only(bottom: width*0.036),
-                child: Center(
-                  child: ButtonTheme(
-                    minWidth: width * 0.8,
-                    height: height * 0.05,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+            ),
+            Text(
+              '퀴즈를 풀기 전 안내사항입니다. \n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요',
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요'),
+            _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤\n다음 문제 버튼을 눌러주세요.'),
+            _buildStep(width, '3. 만점을 향해 도전해보세요!'),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: width * 0.036),
+              child: Center(
+                child: ButtonTheme(
+                  minWidth: width * 0.8,
+                  height: height * 0.05,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: RaisedButton(
+                    child: Text(
+                      '지금 퀴즈 풀기',
+                      style: TextStyle(color: Colors.white),
                     ),
-                    child: RaisedButton(
-                      child: Text(
-                        '지금 퀴즈 풀기',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      color: Colors.deepPurple,
-                      onPressed: () {},
-                    ),
+                    color: Colors.deepPurple,
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => QuizScreen(
+                              quizs: quizes,
+                            ),
+                          ),
+                        );
+                      },
                   ),
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 
-  Widget _buildStep(double width,String title) {
+  Widget _buildStep(double width, String title) {
     return Container(
       padding: EdgeInsets.fromLTRB(
-          width * 0.048,
-          width * 0.024,
-          width * 0.048,
-          width * 0.024,
+        width * 0.048,
+        width * 0.024,
+        width * 0.048,
+        width * 0.024,
       ),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
